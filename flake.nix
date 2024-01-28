6d24ff2c1eede7f5c10fd53a9ee4615563ae6f1b
Vinicius Peixoto <nukelet64@gmail.com>
2024-01-27 23:12:36 -0300

    Add nix stuff

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+    inputs = {
+        naersk.url = "github:nix-community/naersk/master";
+        nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+        utils.url = "github:numtide/flake-utils";
+    };
+
+    outputs = { self, nixpkgs, utils, naersk }:
+        utils.lib.eachDefaultSystem (system:
+        let
+            pkgs = import nixpkgs { inherit system; };
+            naersk-lib = pkgs.callPackage naersk { };
+        in
+        {
+            defaultPackage = naersk-lib.buildPackage {
+                pname = "oximare";
+                version = "0.1";
+                src = ./.;
+                nativeBuildInputs = with pkgs; [ pkg-config ];
+                buildInputs = with pkgs; [ openssl ];
+            };
+
+            devShell = with pkgs; mkShell {
+                buildInputs = [ cargo rustc rustfmt pre-commit rustPackages.clippy openssl pkg-config ];
+                RUST_SRC_PATH = rustPlatform.rustLibSrc;
+            };
+        });
+}
